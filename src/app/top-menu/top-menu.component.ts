33c4a0d8665dbe59c0cacab39606849b93fb8057
BarberOfCoding <mihmegg@gmail.com>
2022-07-27 18:32:02 +0200

    top menu icons added

--- src/app/top-menu/top-menu.component.ts
+++ src/app/top-menu/top-menu.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { faPenToSquare, faBookmark } from '@fortawesome/free-regular-svg-icons';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-top-menu',
   templateUrl: './top-menu.component.html',
-  styleUrls: ['./top-menu.component.scss']
+  styleUrls: ['./top-menu.component.scss'],
 })
 export class TopMenuComponent implements OnInit {
+  PenToSquare = faPenToSquare;
+  bookmark = faBookmark;
+  MagnifyingGlass = faMagnifyingGlass;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
