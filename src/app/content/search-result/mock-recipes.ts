d74248bce3fe6cee838c3fc01a8d0299291c0542
gxo <georgios.xonikis@yokoy.ai>
2022-08-07 15:51:40 +0300

    pagination

---
+++ src/app/content/search-result/mock-recipes.ts
@@ -0,0 +1,272 @@
+export const PASTA_RECIPES = [
+  {
+    "publisher": "BBC Good Food",
+    "image_url": "http://forkify-api.herokuapp.com/images/3309_MEDIUMc520.jpg",
+    "title": "Pasta 'ncasciata (Aubergine wrapped pasta dome)",
+    "id": "5ed6604591c37cdc054bcf01"
+  },
+  {
+    "publisher": "101 Cookbooks",
+    "image_url": "http://forkify-api.herokuapp.com/images/spring_pasta_recipe8286.jpg",
+    "title": "Spring Pasta",
+    "id": "5ed6604591c37cdc054bcd45"
+  },
+  {
+    "publisher": "The Pioneer Woman",
+    "image_url": "http://forkify-api.herokuapp.com/images/carbonara2f55.jpg",
+    "title": "Pasta Carbonara",
+    "id": "5ed6604591c37cdc054bcc4b"
+  },
+  {
+    "publisher": "The Pioneer Woman",
+    "image_url": "http://forkify-api.herokuapp.com/images/seafoodpasta5075.jpg",
+    "title": "Seafood Pasta",
+    "id": "5ed6604591c37cdc054bcc54"
+  },
+  {
+    "publisher": "All Recipes",
+    "image_url": "http://forkify-api.herokuapp.com/images/102079acb4.jpg",
+    "title": "Lobster Pasta",
+    "id": "5ed6604591c37cdc054bc94f"
+  },
+  {
+    "publisher": "All Recipes",
+    "image_url": "http://forkify-api.herokuapp.com/images/4016535bca.jpg",
+    "title": "Sausage Pasta",
+    "id": "5ed6604591c37cdc054bc9d3"
+  },
+  {
+    "publisher": "BBC Good Food",
+    "image_url": "http://forkify-api.herokuapp.com/images/9649_MEDIUM7140.jpg",
+    "title": "Tuna pasta bake",
+    "id": "5ed6604691c37cdc054bd0cf"
+  },
+  {
+    "publisher": "The Pioneer Woman",
+    "image_url": "http://forkify-api.herokuapp.com/images/pestopasta1833.jpg",
+    "title": "Pesto Pasta Salad",
+    "id": "5ed6604691c37cdc054bd0ec"
+  },
+  {
+    "publisher": "The Pioneer Woman",
+    "image_url": "http://forkify-api.herokuapp.com/images/4835843705_abf1ff7779_b9c85.jpg",
+    "title": "Pasta with Pancetta and Leeks",
+    "id": "5ed6604591c37cdc054bcf8a"
+  },
+  {
+    "publisher": "Two Peas and Their Pod",
+    "image_url": "http://forkify-api.herokuapp.com/images/creamyavocadopasta607e.jpg",
+    "title": "Creamy Avocado Pasta",
+    "id": "5ed6604591c37cdc054bce20"
+  },
+  {
+    "publisher": "The Pioneer Woman",
+    "image_url": "http://forkify-api.herokuapp.com/images/pastaallavodkaa870.jpg",
+    "title": "Pasta Alla Vodka",
+    "id": "5ed6604591c37cdc054bccd8"
+  },
+  {
+    "publisher": "The Pioneer Woman",
+    "image_url": "http://forkify-api.herokuapp.com/images/3529272690_08cfa6a74d3a0a.jpg",
+    "title": "Baked Lemon Pasta",
+    "id": "5ed6604591c37cdc054bccce"
+  },
+  {
+    "publisher": "The Pioneer Woman",
+    "image_url": "http://forkify-api.herokuapp.com/images/cajun0676.jpg",
+    "title": "Cajun Chicken Pasta",
+    "id": "5ed6604591c37cdc054bcc70"
+  },
+  {
+    "publisher": "The Pioneer Woman",
+    "image_url": "http://forkify-api.herokuapp.com/images/florentineebc6.jpg",
+    "title": "Chicken Florentine Pasta",
+    "id": "5ed6604591c37cdc054bcc50"
+  },
+  {
+    "publisher": "The Pioneer Woman",
+    "image_url": "http://forkify-api.herokuapp.com/images/artichoke821e.jpg",
+    "title": "Spinach Artichoke Pasta",
+    "id": "5ed6604591c37cdc054bcc3f"
+  },
+  {
+    "publisher": "Real Simple",
+    "image_url": "http://forkify-api.herokuapp.com/images/broccoliturkeypasta_3006c5d34c2.jpg",
+    "title": "Pasta With Turkey and Broccoli",
+    "id": "5ed6604591c37cdc054bcbd7"
+  },
+  {
+    "publisher": "BBC Good Food",
+    "image_url": "http://forkify-api.herokuapp.com/images/3928_MEDIUM477c.jpg",
+    "title": "Asparagus cream pasta",
+    "id": "5ed6604591c37cdc054bca13"
+  },
+  {
+    "publisher": "All Recipes",
+    "image_url": "http://forkify-api.herokuapp.com/images/852330c2a1.jpg",
+    "title": "Awesome Pasta Salad",
+    "id": "5ed6604591c37cdc054bc968"
+  },
+  {
+    "publisher": "All Recipes",
+    "image_url": "http://forkify-api.herokuapp.com/images/43491ae69.jpg",
+    "title": "Angel Chicken Pasta",
+    "id": "5ed6604591c37cdc054bc896"
+  },
+  {
+    "publisher": "All Recipes",
+    "image_url": "http://forkify-api.herokuapp.com/images/647060c56c.jpg",
+    "title": "Greek Pasta Salad I",
+    "id": "5ed6604591c37cdc054bc8dd"
+  },
+  {
+    "publisher": "All Recipes",
+    "image_url": "http://forkify-api.herokuapp.com/images/6556885926.jpg",
+    "title": "Antipasto Pasta Salad",
+    "id": "5ed6604591c37cdc054bc89f"
+  },
+  {
+    "publisher": "BBC Good Food",
+    "image_url": "http://forkify-api.herokuapp.com/images/2358641_MEDIUM43d0.jpg",
+    "title": "Spanish seafood pasta",
+    "id": "5ed6604591c37cdc054bc87e"
+  },
+  {
+    "publisher": "Epicurious",
+    "image_url": "http://forkify-api.herokuapp.com/images/epicuriousfacebook511b.png",
+    "title": "Pasta with Chickpeas and Charred Tomatoes",
+    "id": "5ed6604691c37cdc054bcfdb"
+  },
+  {
+    "publisher": "Chow",
+    "image_url": "http://forkify-api.herokuapp.com/images/29669_italian_pasta_salad_620cf67.jpg",
+    "title": "Italian Pasta Salad Recipe",
+    "id": "5ed6604691c37cdc054bcfd6"
+  },
+  {
+    "publisher": "PBS Food",
+    "image_url": "http://forkify-api.herokuapp.com/images/crabpasta640x360288x162eb7c.jpg",
+    "title": "Crab Pasta and Tomato Sauce",
+    "id": "5ed6604591c37cdc054bce71"
+  },
+  {
+    "publisher": "The Pioneer Woman",
+    "image_url": "http://forkify-api.herokuapp.com/images/cajun3e5c5.jpg",
+    "title": "Surf & Turf Cajun Pasta",
+    "id": "5ed6604591c37cdc054bcc42"
+  },
+  {
+    "publisher": "The Pioneer Woman",
+    "image_url": "http://forkify-api.herokuapp.com/images/pestoa0e7.jpg",
+    "title": "Pasta with Pesto Cream Sauce",
+    "id": "5ed6604591c37cdc054bcc87"
+  },
+  {
+    "publisher": "The Pioneer Woman",
+    "image_url": "http://forkify-api.herokuapp.com/images/TPW_8234ca2c.jpg",
+    "title": "Pasta with Whiskey, Wine, and Mushrooms",
+    "id": "5ed6604591c37cdc054bcc7a"
+  },
+  {
+    "publisher": "All Recipes",
+    "image_url": "http://forkify-api.herokuapp.com/images/580466da88.jpg",
+    "title": "Bacon Ranch Pasta Salad",
+    "id": "5ed6604591c37cdc054bc986"
+  },
+  {
+    "publisher": "Two Peas and Their Pod",
+    "image_url": "http://forkify-api.herokuapp.com/images/SpinachBakedShells53ded.jpg",
+    "title": "Baked Pasta with Spinach and Ricotta",
+    "id": "5ed6604591c37cdc054bc860"
+  },
+  {
+    "publisher": "Bon Appetit",
+    "image_url": "http://forkify-api.herokuapp.com/images/mare_lobster_pasta_with_herbed_cream_sauce_h3bbc.jpg",
+    "title": "Lobster Pasta with Herbed Cream Sauce",
+    "id": "5ed6604591c37cdc054bcdd8"
+  },
+  {
+    "publisher": "The Pioneer Woman",
+    "image_url": "http://forkify-api.herokuapp.com/images/4814287904_bb43e024c9_be8a9.jpg",
+    "title": "Grilled Chicken with Lemon Basil Pasta",
+    "id": "5ed6604591c37cdc054bccb0"
+  },
+  {
+    "publisher": "The Pioneer Woman",
+    "image_url": "http://forkify-api.herokuapp.com/images/5566512470_9e98939ab3_z2766.jpg",
+    "title": "Pasta Salad with Tomatoes, Zucchini, and Feta",
+    "id": "5ed6604591c37cdc054bcc92"
+  },
+  {
+    "publisher": "Smitten Kitchen",
+    "image_url": "http://forkify-api.herokuapp.com/images/3536930521_dcbfce4033c3a0.jpg",
+    "title": "asparagus, goat cheese and lemon pasta",
+    "id": "5ed6604591c37cdc054bcdbf"
+  },
+  {
+    "publisher": "The Pioneer Woman",
+    "image_url": "http://forkify-api.herokuapp.com/images/PastaCarbonara061c.jpg",
+    "title": "Pastor Ryan’s Pasta Carbonara",
+    "id": "5ed6604591c37cdc054bccde"
+  },
+  {
+    "publisher": "Epicurious",
+    "image_url": "http://forkify-api.herokuapp.com/images/5110822007d1.jpg",
+    "title": "Pasta Salad with Melon, Pancetta, and Ricotta Salata",
+    "id": "5ed6604691c37cdc054bd06f"
+  },
+  {
+    "publisher": "The Pioneer Woman",
+    "image_url": "http://forkify-api.herokuapp.com/images/yum8ee6.jpg",
+    "title": "Quick and Easy Roasted Red Pepper Pasta",
+    "id": "5ed6604691c37cdc054bd02e"
+  },
+  {
+    "publisher": "Two Peas and Their Pod",
+    "image_url": "http://forkify-api.herokuapp.com/images/roastedredpepperpennepasta20476.jpg",
+    "title": "Pasta with Roasted Red Pepper Tomato Sauce",
+    "id": "5ed6604591c37cdc054bce22"
+  },
+  {
+    "publisher": "Two Peas and Their Pod",
+    "image_url": "http://forkify-api.herokuapp.com/images/SouthwesternPastaSalad28c66.jpg",
+    "title": "Southwestern Pasta Salad with Creamy Avocado Dressing",
+    "id": "5ed6604591c37cdc054bcdff"
+  },
+  {
+    "publisher": "Bon Appetit",
+    "image_url": "http://forkify-api.herokuapp.com/images/pastasaladwithmelonpancettaandricottasalata646b395.jpg",
+    "title": "Pasta Salad with Melon, Pancetta, and Ricotta Salata",
+    "id": "5ed6604591c37cdc054bcd71"
+  },
+  {
+    "publisher": "Framed Cooks",
+    "image_url": "http://forkify-api.herokuapp.com/images/icQLFQ22M730b.jpg",
+    "title": "Chilled Seafood Pasta Salad with Ginger Yogurt Dressing",
+    "id": "5ed6604691c37cdc054bd0d7"
+  },
+  {
+    "publisher": "Two Peas and Their Pod",
+    "image_url": "http://forkify-api.herokuapp.com/images/CreamyGoatCheeseSpinachandMushroomPasta3b4ca.jpg",
+    "title": "Creamy Goat Cheese Pasta with Spinach and Roasted Mushrooms",
+    "id": "5ed6604591c37cdc054bcdf4"
+  },
+  {
+    "publisher": "The Pioneer Woman",
+    "image_url": "http://forkify-api.herokuapp.com/images/5842229930_73a968f08e_zab23.jpg",
+    "title": "Spicy Pasta Salad with Smoked Gouda, Tomatoes, and Basil",
+    "id": "5ed6604591c37cdc054bcc86"
+  },
+  {
+    "publisher": "BBC Good Food",
+    "image_url": "http://forkify-api.herokuapp.com/images/2320662_MEDIUM54b4.jpg",
+    "title": "Creamy chicken &amp; green bean pesto pasta",
+    "id": "5ed6604591c37cdc054bcc22"
+  },
+  {
+    "publisher": "Closet Cooking",
+    "image_url": "http://forkify-api.herokuapp.com/images/Roasted2BRed2BPepper2Band2BGoat2BCheese2BAlfredo2BPasta2B5002B210184a51238.jpg",
+    "title": "Roasted Red Pepper and Goat Cheese Alfredo Pasta",
+    "id": "5ed6604591c37cdc054bcad5"
+  }
+];
--- src/app/content/search-result/search-result.component.ts
+++ src/app/content/search-result/search-result.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import {PASTA_RECIPES} from "./mock-recipes";
+
+interface Recipe {
+  id: string;
+  image_url: string;
+  publisher: string
+  title: string;
+}
 
 @Component({
   selector: 'app-search-result',
@@ -7,9 +15,37 @@ import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./search-result.component.scss'],
 })
 export class SearchResult implements OnInit {
-  ArrowRight = faArrowRight;
+  readonly recipes: Recipe[] = PASTA_RECIPES;
+  currentPage: number = 1;
+  maxPage!: number;
 
-  constructor() {}
+  arrowLeft = faArrowLeft;
+  arrowRight = faArrowRight;
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.maxPage = Math.trunc(this.recipes.length / 10) + 1;
+  }
+
+  decrementCurrentPage(): void {
+    this.currentPage--;
+  }
+
+  incrementCurrentPage(): void {
+    this.currentPage++;
+  }
+
+  getPaginatedResults(): Recipe[] {
+    const clonedRecipes = [...PASTA_RECIPES];
+
+    return clonedRecipes.slice(0, 9);
+
+    // DIAVAZEIS TO CURRENT PAGE
+    // PERNEIS TO SLICE TOU RECIPES ANALOGA APO TO CURRENT PAGE
+    // PX EAN currentPage = 1 => SLICE TOU ARRAY 0-9;
+    // PX EAN currentPage = 3 => SLICE TOU ARRAY 20-29;
+    return [];
+  }
+
+
 }
+
