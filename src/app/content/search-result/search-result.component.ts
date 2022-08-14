9b1391f2fb4e39f6ff3c3aa27340a9e572b3df42
BarberOfCoding <mihmegg@gmail.com>
2022-08-14 11:44:36 +0200

    pagination method

--- src/app/content/search-result/search-result.component.ts
+++ src/app/content/search-result/search-result.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
-import {PASTA_RECIPES} from "./mock-recipes";
+import { PASTA_RECIPES } from './mock-recipes';
 
 interface Recipe {
   id: string;
   image_url: string;
-  publisher: string
+  publisher: string;
   title: string;
 }
 
@@ -17,13 +17,14 @@ interface Recipe {
 export class SearchResult implements OnInit {
   readonly recipes: Recipe[] = PASTA_RECIPES;
   currentPage: number = 1;
+  pageSize: number = 10;
   maxPage!: number;
 
   arrowLeft = faArrowLeft;
   arrowRight = faArrowRight;
 
   ngOnInit() {
-    this.maxPage = Math.trunc(this.recipes.length / 10) + 1;
+    this.maxPage = Math.ceil(this.recipes.length / this.pageSize) + 1;
   }
 
   decrementCurrentPage(): void {
@@ -40,12 +41,15 @@ export class SearchResult implements OnInit {
     return clonedRecipes.slice(0, 9);
 
     // DIAVAZEIS TO CURRENT PAGE
-    // PERNEIS TO SLICE TOU RECIPES ANALOGA APO TO CURRENT PAGE
-    // PX EAN currentPage = 1 => SLICE TOU ARRAY 0-9;
-    // PX EAN currentPage = 3 => SLICE TOU ARRAY 20-29;
-    return [];
+    // function(this.recipes, this.currentPage) {
+    //   return this.recipes.slice((this.currentPage-1)*this.pageSize,(this.currentPage * this.pageSize)-1)
+    // for(this.currentPage = 1, this.currentPage <= this.maxPage, this.currentPage++){
+    //   this.recipes.slice((this.currentPage-1)*this.pageSize,(this.currentPage * this.pageSize)-1)
   }
 
-
+  // }
+  // PERNEIS TO SLICE TOU RECIPES ANALOGA APO TO CURRENT PAGE
+  // PX EAN currentPage = 1 => SLICE TOU ARRAY 0-9;
+  // PX EAN currentPage = 3 => SLICE TOU ARRAY 20-29;
+  // return [];
 }
-
