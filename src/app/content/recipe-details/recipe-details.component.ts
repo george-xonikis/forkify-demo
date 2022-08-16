754050c90d15f1f1ee333f6f1a5947848c23cc20
BarberOfCoding <mihmegg@gmail.com>
2022-08-16 15:53:34 +0200

    recipe details html

--- src/app/content/recipe-details/recipe-details.component.ts
+++ src/app/content/recipe-details/recipe-details.component.ts
@@ -1,5 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { faFaceSmile } from '@fortawesome/free-regular-svg-icons';
+import {
+  faFaceSmile,
+  faClock,
+  faBookmark,
+} from '@fortawesome/free-regular-svg-icons';
+import { faUserGroup } from '@fortawesome/free-solid-svg-icons';
+
+export interface RecipeDetails {
+  id: string;
+  title: string;
+  publisher: string;
+  sourceUrl: string;
+  image_url: string;
+  servings: number;
+  ingredients: string;
+}
 
 @Component({
   selector: 'app-recipe-details',
@@ -8,6 +23,9 @@ import { faFaceSmile } from '@fortawesome/free-regular-svg-icons';
 })
 export class RecipeDetailsComponent implements OnInit {
   smileFace = faFaceSmile;
+  clock = faClock;
+  bookmark = faBookmark;
+  persons = faUserGroup;
 
   constructor() {}
 
